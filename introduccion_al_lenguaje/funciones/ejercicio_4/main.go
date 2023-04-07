eaa6af48d4b6b67f1e36fb72573f2a1dbb96cc26
“rodrigocarsin” <rodrigo.carsin@mercadolibre.com>
2023-04-07 17:11:28 -0300

    cambios de estructura

--- introduccion_al_lenguaje/funciones/ejercicio_4/main.go
+++ introduccion_al_lenguaje/funciones/ejercicio_4/main.go
@@ -1,5 +1,16 @@
 package main
 
+/*Ejercicio 4 - Calcular estadísticas
+
+Los profesores de una universidad de Colombia necesitan calcular algunas estadísticas de
+calificaciones de los/as estudiantes de un curso. Requieren calcular los valores mínimo,
+máximo y promedio de sus calificaciones.
+Para eso, se solicita generar una función que indique qué tipo de cálculo se quiere
+realizar (mínimo, máximo o promedio) y que devuelva otra función y un mensaje (en caso
+que el cálculo no esté definido) que se le pueda pasar una cantidad N de enteros y
+devuelva el cálculo que se indicó en la función anterior.
+*/
+
 import "fmt"
 
 const (
